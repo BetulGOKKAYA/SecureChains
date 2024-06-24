51c4aa9b70e74189df14642fa8e5d045f1d616ec
BetulGOKKAYA <30667123+BetulGOKKAYA@users.noreply.github.com>
2024-06-24 22:44:25 +0100

    Update admin.py
    Controls -> Control

--- apps/assessment_results/admin.py
+++ apps/assessment_results/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import Risk_Result, Controls, Main_Risk_Result
+from .models import Risk_Result, Control, Main_Risk_Result
 
 admin.site.register(Risk_Result)
-admin.site.register(Controls)
+admin.site.register(Control)
 admin.site.register(Main_Risk_Result)
 
-
-
-# Register your models here.
